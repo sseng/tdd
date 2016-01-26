75bf1c12982b7c7a8d5feb82eab2ce98bc64001b
Sothearith Seng <sseng81@gmail.com>
2016-01-25 16:31:00 -0800

    refactor2

--- functional_tests.py
+++ functional_tests.py
@@ -10,31 +10,45 @@ class NewVisitorTest(unittest.TestCase):
     
     def tearDown(self):
         self.browser.quit()
-        
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+    
     def test_can_start_a_list_and_retrieve_it_later(self):
+        #homepage
         self.browser.get('http://localhost:8000')
-        
-        #page and title mention to-do list
+
+        #page title & header mention to-do list
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        
+
         #enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'enter a to-do item'
+        )
+
+        #type buy feathers into text box
+        inputbox.send_keys('buy feathers')
+        
+        #input into text box
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy feathers')
+
+        #another text box to add another item
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        
-        #when user hits enter, the page updates, and now the page lists '1: buy feathers' as an item in the to-do list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: buy feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use feathers to make a fly',
-            [row.text for row in rows]
-        )
-        
+
+        #page updates again, and now shows both items in the list
+        self.check_for_row_in_list_table('1: Buy feathers')
+        self.check_for_row_in_list_table('2: Use feathers to make a fly')
+
         self.fail('Finish the test!')
-        
-        
+
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
